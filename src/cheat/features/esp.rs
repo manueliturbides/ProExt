3327744ea214a620ebe732d2564c83fc1513fbcb
manueliturbides <manueliturbides@users.noreply.github.com>
2025-07-28 02:57:05 -0300

    Fix infinite loop

--- src/cheat/features/esp.rs
+++ src/cheat/features/esp.rs
@@ -406,3 +406,4 @@ pub fn render_headshot_line(ui: &mut Ui, window_width: i32, window_height: i32, 
     rectangle(ui, Vector2 { x: pos.x + 5.0, y: pos.y - 1.0 }, Vector2 { x: 17.0, y: 3.0 }, color_with_masked_alpha(config.esp.headshot_line_color, 0xFF000000).into(), 1.0, 0, true);
     rectangle(ui, Vector2 { x: pos.x + 6.0, y: pos.y }, Vector2 { x: 17.0, y: 3.0 }, color_u32_to_f32(config.esp.headshot_line_color).into(), 1.0, 0, true);
 }
+
